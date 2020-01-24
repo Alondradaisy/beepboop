0bd0578f9d998cd7e5abed0816afccf7f027b1bb
alondra warren <alondra.warren96@gmail.com>
2020-01-24 15:26:53 -0800

    continue wrestling with jquery functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,23 @@
-// business logic 
-
-$(document).ready(function() {
-  var inputtedNumber = ("application");
-  application(submit);
-
-
-
-});
-function application() {
-  $("id").submit("application");
-}
-
-
-
-// user interface logic
-
-
-
 $(document).ready(function() {
   $("form#application").submit(function(event) {
     event.preventDefault();
-    var application = $("input#application").val();
-    var result = application(inputtedNumber);
-    $("#result").submit(result);
-  
+
+
+    var numberGiven = parseInt($("input#application").val());
+    var result = application(number);
+    $("#result").text(result);
+
+    $("#application").text(application);
+
+    var application
+      if ("numberGiven <= "4") {
+        return ["0", "Beep!","Boop!","I'm sorry, Dave. I'm afraid I can't do that", "4"]
+      }
+        else {
+          return result;
+      }
+
+
+
   });
 });
