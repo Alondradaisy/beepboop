97f9481cd7ec2b19bf625e1a51bc0dfa766431f7
alondra warren <alondra.warren96@gmail.com>
2020-01-24 16:44:39 -0800

    modify jquery function

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,31 @@
 $(document).ready(function() {
   $("form#application").submit(function(event) {
-    var application = 
+    var application = all 
     var numberGiven = parseInt($("input#application").val()); 
     var result = application(numberGiven);
     $("#result").submit(result);
 
 
     var application
-      if ("numberGiven <= "4") {
+      if ("numberGiven <= 4") {
         return ["0", "Beep!","Boop!","I'm sorry, Dave. I'm afraid I can't do that", "4"];
       }
         else {
           return yes;
       }
 
-      event.preventDefault();
+      
 
      var application 
-     if (numberGiven > "3") {
+     if ("numberGiven > 3") {
        return ["0", "Beep!", "Boop!", "I'm sorry, Dave. I'm afraid I can't do that", "4", "5"];
      }
 
      var application
-      if ("numberGiven")
+      if ("numberGiven = 2") {
+        return "Boop!";
+      }
 
-
-
-
-
+    event.preventDefault();
   });
 });
